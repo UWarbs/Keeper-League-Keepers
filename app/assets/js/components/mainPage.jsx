71865111bf011e2f540e82dd6bd3287888db7441
Kyle <kyle.warbis@gmail.com>
2015-11-02 16:13:19 -0800

    add ability to select player from search and see their data. When clicked on the dropdown, the list goes away.

--- app/assets/js/components/mainPage.jsx
+++ app/assets/js/components/mainPage.jsx
@@ -4,17 +4,28 @@ var PlayerStore = require('../stores/PlayerStore');
 var PlayerSearch = require('./playerSearch.jsx');
 
 var MainPage = React.createClass({
+	
 	getInitialState: function() {
 		return {
 			players: PlayerStore.getPlayers(),
-			playerSelected: false
+			playerSelected: false,
+			player: null
 		};
 	},
+
+	handlePlayerSelect: function(id) {
+		var player = PlayerStore.getSinglePlayer(id);
+		this.setState({
+			playerSelected: true,
+			player: player
+		});
+	},
+
 	render: function() {
 		return (
 			<div className="main-page-container">
 				<h2 className="marketing-copy">Welcome to Keeper League Keepers, the premier Fantasy Football keeper league site.</h2>
-				<PlayerSearch playerList={this.state.players.players} playerSelected={this.state.playerSelected} />
+				<PlayerSearch playerList={this.state.players.players} playerSelected={this.state.playerSelected} handlePlayerSelect={this.handlePlayerSelect} player={this.state.player}/>
 			</div>
 		)
 	}
--- app/assets/js/components/playerSearch.jsx
+++ app/assets/js/components/playerSearch.jsx
@@ -5,25 +5,42 @@ var React = require('react');
 var SearchInput = require('react-search-input');
 
 var PlayerStore = require('../stores/PlayerStore');
+var PlayerCard  = require('../components/PlayerCard.jsx');
 
 var PlayerName = React.createClass({
+
 	render: function() {
+	console.log('player props');
+	console.log(this.props);
 		return (
       <li onClick={this.getPlayer}>{this.props.firstName} {this.props.lastName}</li>
     )
 	},
 	
 	getPlayer: function() {
-		console.log(PlayerStore.getSinglePlayer(this.props.id));
-		//Somehow set top-level state from here
+		this.props.handlePlayerSelect(this.props.id);
+		this.props.resetSearch();
 	}
 });
 
 var PlayerSearch = React.createClass({
+	getInitialState: function() {
+		return {
+			searchTerm: ''
+		}
+	},
+
+
+
 	render: function() {
+		var selectedFunc = this.props.handlePlayerSelect;
+		var resetFunc = this.resetSearch;
+		var selectedPlayer = this.props.player;
 		var potentialPlayers = this.props.playerList;
 		var fullPlayerList = [];
 		var playerNameList = [];
+		
+
 		
 		if (this.refs.search) {
 			if (this.refs.search.state.searchTerm == '') { //pull request for this?
@@ -32,31 +49,39 @@ var PlayerSearch = React.createClass({
 				var filters = ['firstName', 'lastName'];
 				fullPlayerList = potentialPlayers.filter(this.refs.search.filter(filters));
 				fullPlayerList.forEach(function(player, index, array) {
-					playerNameList.push(<PlayerName id={player.id} firstName={player.firstName} lastName={player.lastName} />);
-					// playerNameList.push(player);
+					playerNameList.push(<PlayerName id={player.id} firstName={player.firstName} lastName={player.lastName} handlePlayerSelect={selectedFunc} resetSearch={resetFunc} />);
 				});
-				console.log(playerNameList);
 			}
 		}
-/*					{playerNameList.map(function(result) {
-          	return <li key={result.id} onClick={this.handleClick.bind(this)}>{result.firstName}&nbsp;{result.lastName}</li>;
-        	})}
-*/
+
+		if(this.state.searchTerm == '') {
+			playerNameList = [];
+		}
+		
+		if (selectedPlayer) {
+			var playerCard = <PlayerCard player={selectedPlayer} />
+		}
+
 		return (
 			<div className="player-search-container">
 				<h3 className="player-search-copy">Search any player name to get our analysts' in-depth opinion on their keeper league worth.</h3>
 				<SearchInput className="player-search-input" type="text" placeholder="e.g Russell Wilson" ref='search' onChange={this.searchUpdated} defaultStyle={false} />
-				<div className="player-search-go" onClick={this.searchUpdated}>GO</div>
 				<ul className="player-result-list">
 					{playerNameList}
 				</ul>
+				{playerCard}
 			</div>
 		)
 	},	
 
+	resetSearch: function() {
+		this.setState({searchTerm: ''});
+	},
+
 	searchUpdated: function(term) {
 		this.setState({searchTerm: term}); //forces re-render
 	}
+
 });
 
 module.exports = PlayerSearch;
