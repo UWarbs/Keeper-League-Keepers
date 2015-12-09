bf74349a27452e73cf731481cbecd9c27768d425
Kyle <kyle.warbis@gmail.com>
2015-12-09 13:58:04 -0800

    add api folder, remove sequelize

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,31 +1,65 @@
 'use strict';
-
-//Site Routes
-var Joi = require('joi');
+var Joi    = require('joi');
+var Player = require('../models/Player');
 
 exports.register = function(server, options, next) {
 	//Declare Routes
-	
 	server.route([
-		// Add a route to serve static assets (CSS, JS, IMG)
-	  {
-	    method: 'GET',
-	    path: '/{param*}',
-	    handler: {
-	      directory: {
-	        path: 'dist',
-	        index: ['index.html']
-	      }
-	    },
-   	},
+		{
+			// Add a route to serve static assets (CSS, JS, IMG)
+			method: 'GET',
+			path: '/{param*}',
+			handler: {
+				directory: {
+					path: 'dist',
+					index: ['index.html']
+				}
+			},
+		},
 	  // Add main app route
 	  {
-		    method: 'GET',
-		    path: '/',
-		    handler: {
-		    	view: 'default'
-		    }
-	  }
+	  	method: 'GET',
+	  	path: '/',
+	  	handler: {
+	  		view: 'default'
+	  	}
+	  },
+	  
+	  {
+	    method: 'GET',
+		  path: '/api',
+		  handler: function(req, res) {
+		  	models.Player.findAll({
+		  		attributes: ['firstName', 'lastName']
+		  	}).then(function(players) {
+		  		res({'players': players});
+		  	});
+		  }
+		},
+
+		{
+			method: 'POST',
+			path: '/api/new-writeup',
+			handler: function(req, res) {
+				res('worked');
+				Player.create({
+					id: 5,
+					firstName:'Blaine',
+					lastName: 'Gabbert',
+					age: 26,
+					positionalRating: 80,
+					position: 'Quarter Back',
+					positionAbbrev: 'QB',
+					team: 'San Francisco 49ers',
+					teamAbbrev: 'SF'
+				}).then(function(player) {
+					res('worked');
+					console.log(player.get({plain: true}));
+				});
+				res('worked');
+			}
+		}
+
 	]);
 	next();
 }
--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 var Hapi       = require('hapi');
 var dateFormat = require('dateformat');
 var request    = require('request');
+var routes 	 	 = require('./api/routes/routes');
 var format     = "dd mmm HH:MM:ss";
-
+var DB_PORT    = process.env.NODE_PORT;
+var Player     = require('./api/models/Player');
 require('babel-core/register')({
     presets: ['react']
 });
 
 // Basic Hapi.js connection stuff
 var server = new Hapi.Server({debug: {request: ['info', 'error']}});
-
 
 // Expose database
 if (process.env.NODE_ENV === 'development') {
@@ -37,69 +38,19 @@ var plugins = [
 	}
 ];
 
-//DEPRECATED
-// server.register([
-// {
-//   register: require('inert')
-// }, 
-// {
-//   register: require('vision')
-// }
-// ], function(err) {
-
-//   if (err) return console.error(err);
-
-//     // Add the React-rendering view engine
-//     server.views({
-//         engines: {
-//             jsx: require('hapi-react-views')
-//         },
-//         relativeTo: __dirname,
-//         path: 'app/views'
-//     });
-
-//     // Add a route to serve static assets (CSS, JS, IMG)
-//     server.route({
-//       method: 'GET',
-//       path: '/{param*}',
-//       handler: {
-//         directory: {
-//           path: 'dist',
-//           index: ['index.html']
-//         }
-//       }
-//     });
-
-//     // Add main app route
-//     server.route({
-//       method: 'GET',
-//       path: '/',
-//       handler: {
-//       	view: 'default'
-//       }
-//     });
-
-//     server.start(function() {
-//       console.log(dateFormat(new Date(), format) + ' - Server started at: ' + server.info.uri);
-//     });
-
-// });
-
 server.register(plugins, function (err) {
-    if (err) { throw err; }
-    
-    // Add the React-rendering view engine
-    server.views({
-        engines: {
-            jsx: require('hapi-react-views')
-        },
-        relativeTo: __dirname,
-        path: 'app/views'
-    });
-
-    server.start(function(err) {
-        if (err) { throw err; }
-
-        server.log('info', 'Server running at: ' + server.info.uri);
-    });
+  if (err) { throw err; }
+  // Add the React-rendering view engine
+  server.views({
+      engines: {
+          jsx: require('hapi-react-views')
+      },
+      relativeTo: __dirname,
+      path: 'app/views'
+  });
+	  
+	server.start(function(err) {
+	  if (err) { console.log('start errror'); throw err; }
+	  server.log('info', 'Server running at: ' + server.info.uri);
+	});
 });
