40ef7f7c528cb5eabba1665de846f9cea68663f5
Kyle <kyle.warbis@gmail.com>
2015-12-17 11:19:48 -0800

    db config

--- api/config/schema.js
+++ api/config/schema.js
@@ -2,7 +2,7 @@
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    host     : '127.0.0.1',
+    host     : 'mysql://b125826682b977:f3b613d0@us-cdbr-iron-east-03.cleardb.net/heroku_7df5265db2aa61f?reconnect=true',
     user     : 'root',
     password : 'root',
     database : 'keepers'
